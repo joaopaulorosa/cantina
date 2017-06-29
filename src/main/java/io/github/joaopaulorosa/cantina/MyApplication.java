dbb6935e588f6b7e4528ed9c7f1d0a29b646bca3
joaorosa <joaopaulo87@gmail.com>
2017-06-29 18:54:26 -0300

    init

---
+++ src/main/java/io/github/joaopaulorosa/cantina/MyApplication.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.joaopaulorosa.cantina;
+
+import javax.ws.rs.ApplicationPath;
+import org.glassfish.jersey.server.ResourceConfig;
+
+/**
+ *
+ * @author joao
+ */
+@ApplicationPath("rest")
+public class MyApplication extends ResourceConfig{
+    public MyApplication(){
+        packages("io.github.joaopaulorosa.cantina.controllers");
+    }
+}
